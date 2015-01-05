0b589c3e74dba921a32b38e8609ff827177326cd
jaehue <jaehue@jang.io>
2015-01-05 23:44:59 +0900

    add comment controller

---
+++ app/controllers/comment.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+	"github.com/revel/revel/modules/db/app"
+	"goblog/app/routes"
+	"time"
+)
+
+type Comment struct {
+	*revel.Controller
+	db.Transactional
+}
+
+func (c Comment) Create(postId int, body, commenter string) revel.Result {
+	_, err := c.Txn.Exec("insert into comments(body, commenter, post_id, created_at, updated_at) values(?,?,?,?,?)", body, commenter, postId, time.Now(), time.Now())
+	if err != nil {
+		panic(err)
+	}
+
+	return c.Redirect(routes.Post.Show(postId))
+}
