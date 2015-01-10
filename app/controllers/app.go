489d7af87ddab1066f8c439d80cab3f4fb07b5a7
jaehue <jaehue@jang.io>
2015-01-10 21:24:40 +0900

    remove login process. add CurrentUser to App controller.

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,48 +1,11 @@
 package controllers
 
 import (
-	"code.google.com/p/go.crypto/bcrypt"
-	"github.com/revel/revel"
 	"goblog/app/models"
 )
 
 type App struct {
 	GormController
+	CurrentUser *models.User
 }
 
-func (c App) Login() revel.Result {
-	return c.Render()
-}
-
-func (c App) CreateSession(username, password string) revel.Result {
-	var user models.User
-	c.Txn.Where(&models.User{Username: username}).First(&user)
-
-	err := bcrypt.CompareHashAndPassword(user.Password, []byte(password))
-	if err == nil {
-		c.Session["username"] = user.Username
-		c.Session["role"] = user.Role
-		c.Session["name"] = user.Name
-		if user.Role == "admin" {
-			c.Session["isAdmin"] = "true"
-		}
-		c.Flash.Success("Welcome, " + user.Name)
-		return c.Redirect(Post.Index)
-	}
-
-	// clear session
-	for k := range c.Session {
-		delete(c.Session, k)
-	}
-	c.Flash.Out["username"] = username
-	c.Flash.Error("Login failed")
-	return c.Redirect(Home.Index)
-}
-
-func (c App) DestroySession() revel.Result {
-	// clear session
-	for k := range c.Session {
-		delete(c.Session, k)
-	}
-	return c.Redirect(Home.Index)
-}
